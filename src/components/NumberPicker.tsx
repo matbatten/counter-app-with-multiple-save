a490bf66634d6e644a6ff2afd7fe923b35c79439
matbat10 <97300316+matbatten@users.noreply.github.com>
2022-03-10 09:00:05 +0000

    conventional use state naming

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
 export default function NumberPicker(): JSX.Element {
-  const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] =
+  const [counterValue, setCounterValue] =
     useState(0);
-  const [storedValuesFromCurrentRender, queueRerenderWithNewStoredValues] =
+  const [storedValues, setStoredValues] =
     useState<number[]>([]);
 
   const handleAddOneToCounter = () => {
-    queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
+    setCounterValue(counterValue + 1);
   };
 
   const handleSubtractOneFromCounter = () => {
-    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+    setCounterValue(counterValue - 1);
   };
 
   const handleStoreCurrentCount = () => {
-    queueRerenderWithNewStoredValues([
-      ...storedValuesFromCurrentRender,
-      counterValueFromCurrentRender,
+    setStoredValues([
+      ...storedValues,
+      counterValue,
     ]);
   };
 
   return (
     <>
       <h1>Number picker</h1>
-      <p>Your stored numbers: {storedValuesFromCurrentRender.join(", ")}</p>
-      <p>Counter: {counterValueFromCurrentRender}</p>
+      <p>Your stored numbers: {storedValues.join(", ")}</p>
+      <p>Counter: {counterValue}</p>
       <button onClick={handleSubtractOneFromCounter}>-1</button>
       <button onClick={handleAddOneToCounter}>+1</button>
       <hr />
